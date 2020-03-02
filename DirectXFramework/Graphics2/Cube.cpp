9862753962dd29388091bd3de8eddc0a1c05fa9f
CritneySpears <44713251+CritneySpears@users.noreply.github.com>
2020-03-02 12:59:15 +0000

    Progress wk3

--- DirectXFramework/Graphics2/Cube.cpp
+++ DirectXFramework/Graphics2/Cube.cpp
@@ -1,6 +1,11 @@
 #include "Cube.h"
 
-Cube::Cube(wstring name, ComPtr<ID3D11ShaderResourceView> texture)
+Cube::Cube() : SceneNode(L"Cube")
+{
+
+}
+
+Cube::Cube(wstring name, ComPtr<ID3D11ShaderResourceView> texture) : SceneNode(name)
 {
 	_name = name;
 	_texture = texture;
@@ -21,9 +26,19 @@ bool Cube::Initialise()
 	ComPtr<ID3D11DeviceContext> deviceContext = 
 		DirectXFramework::GetDXFramework()->GetDeviceContext();
 
+	// The call to CoInitializeEx is needed if we are using
+	// textures since the WIC library used requires it
+	CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
 	BuildGeometryBuffers();
 	BuildShaders();
 	BuildVertexLayout();
 	BuildConstantBuffer();
 	BuildTexture();
+
+	return true;
+}
+
+void Cube::Render()
+{
 }
--- DirectXFramework/Graphics2/Cube.h
+++ DirectXFramework/Graphics2/Cube.h
@@ -7,16 +7,19 @@ class Cube : public SceneNode
 {
 public:
 
-	Cube(wstring name, ComPtr<ID3D11ShaderResourceView> texture);
+	Cube() : SceneNode(L"Cube") {};
+	Cube(wstring name, ComPtr<ID3D11ShaderResourceView> texture) : SceneNode(name) {};
 	~Cube();
 
 	virtual bool Initialise();
 	virtual void Render();
-	virtual void Shutdown();
 
 private:
+
+	ComPtr<ID3D11Device>			_device = DirectXFramework::GetDXFramework()->GetDevice();
+	ComPtr<ID3D11DeviceContext>		_deviceContext = DirectXFramework::GetDXFramework()->GetDeviceContext();
+
 	ComPtr<ID3D11ShaderResourceView> _texture;;
 	wstring _name;
-	COLORREF _colour;
 };
 
