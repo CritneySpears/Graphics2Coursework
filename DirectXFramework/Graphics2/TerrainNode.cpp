4a7a85eba7a3be7f62eb8b52e692297af03d7ee8
CritneySpears <44713251+CritneySpears@users.noreply.github.com>
2020-04-08 16:56:33 +0100

    Further Fiddling

--- DirectXFramework/Graphics2/TerrainNode.cpp
+++ DirectXFramework/Graphics2/TerrainNode.cpp
@@ -32,6 +32,15 @@ bool TerrainNode::Initialise()
 
 void TerrainNode::Render()
 {
+	CBUFFER cBuffer;
+	cBuffer.WorldTransformation = XMLoadFloat4x4(&_combinedWorldTransformation);
+	//cBuffer.AmbientColour = XMFLOAT4(0.1f, 0.1f, 0.1f, 1.0f);
+	//cBuffer.LightVector = XMVector4Normalize(XMVectorSet(0.0f, 01.0f, 1.0f, 0.0f));
+	//cBuffer.LightColour = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+
+	// Update the constant buffer 
+	_deviceContext->VSSetConstantBuffers(0, 1, _constantBuffer.GetAddressOf());
+	_deviceContext->UpdateSubresource(_constantBuffer.Get(), 0, 0, &cBuffer, 0, 0);
 	_deviceContext->VSSetShader(_vertexShader.Get(), 0, 0);
 	_deviceContext->PSSetShader(_pixelShader.Get(), 0, 0);
 	_deviceContext->IASetInputLayout(_layout.Get());
